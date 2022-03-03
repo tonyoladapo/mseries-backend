b38f65a2b6b8754d79b3c0a6e68b3ac5892e2ce0
Tony <toneyobaze@gmail.com>
2022-03-03 17:39:27 +0000

    create sync route

--- src/routes/show.js
+++ src/routes/show.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const { unwatchedController } = require("../controllers/show");
+const { unwatchedController, syncController } = require("../controllers/show");
 
 const router = express.Router();
 
 router.get("/unwatched/:showId", unwatchedController);
+router.get("/sync", syncController);
 
 module.exports = router;
